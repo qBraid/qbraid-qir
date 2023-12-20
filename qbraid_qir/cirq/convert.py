90f9139aa5272a22b9bdc8d2b29bf8a3a91e6042
Rohan Jain <rjain@imsa.edu>
2023-12-20 12:20:02 -0600

    waiting for opsets to be defined

--- qbraid_qir/cirq/convert.py
+++ qbraid_qir/cirq/convert.py
@@ -14,12 +14,15 @@ Module containing Cirq to qBraid QIR conversion functions
 """
 from typing import Optional
 
+import numpy as np
+
 import cirq
 from pyqir import Context, Module, qir_module
 
 from qbraid_qir.cirq.elements import CirqModule, generate_module_id
 from qbraid_qir.cirq.visitor import BasicQisVisitor
 from qbraid_qir.exceptions import QirConversionError
+from qbraid_qir.cirq.opsets import CIRQ_GATES
 
 
 def cirq_to_qir(circuit: cirq.Circuit, name: Optional[str] = None, **kwargs) -> Module:
@@ -43,10 +46,38 @@ def cirq_to_qir(circuit: cirq.Circuit, name: Optional[str] = None, **kwargs) -> 
     if name is None:
         name = generate_module_id(circuit)
 
+    # create a variable for circuit.unitary that we will use to create assertions later
+    input_unitary = circuit.unitary()
+    
+    # do some preprocessing here that ensures that the circuit is composed only of supported gates and operation:
+
+    # for moment in circuit:
+    #     for op in moment:
+    
+    
+    # according to the gateset in CIRQ_GATES, perform gate decomposition;
+    for moment in circuit:
+        for op in moment:
+            if op.gate in CIRQ_GATES:
+                op.gate = CIRQ_GATES[op.gate]
+                op.gate.validate_args(op.qubits)
+            else:
+                raise QirConversionError(f"Unsupported gate {op.gate} in circuit.")
+    
+    
+    # ensure that input/output circuit.unitary() are equivalent.
+    output_unitary = circuit.unitary()
+    if not np.allclose(input_unitary, output_unitary):
+        raise QirConversionError("Cirq circuit unitary changed during conversion.")
+
+
     llvm_module = qir_module(Context(), name)
     module = CirqModule.from_circuit(circuit, llvm_module)
+
+
     visitor = BasicQisVisitor(**kwargs)
     module.accept(visitor)
+
     err = llvm_module.verify()
     if err is not None:
         raise QirConversionError(err)
