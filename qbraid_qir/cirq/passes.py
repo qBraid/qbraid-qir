68b227a168e673df2c79f9e6b0ee27a1c8a35121
Rohan Jain <rjain@imsa.edu>
2024-01-09 14:35:49 -0600

    added files

---
+++ qbraid_qir/cirq/passes.py
@@ -0,0 +1,77 @@
+# Copyright (C) 2023 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module for processing Cirq circuits before conversion to QIR.
+
+"""
+import itertools
+from typing import List
+
+import cirq
+import qbraid.programs.cirq
+
+from qbraid_qir.cirq.opsets import map_cirq_op_to_pyqir_callable
+from qbraid_qir.exceptions import QirConversionError
+
+
+def _decompose_gate_op(op: cirq.GateOperation) -> List[cirq.OP_TREE]:
+    try:
+        # Try converting to PyQIR. If successful, keep the operation.
+        _ = map_cirq_op_to_pyqir_callable(op)
+        return [op]
+    except QirConversionError:
+        pass
+    new_ops = cirq.decompose_once(op, flatten=True, default=[op])
+    if len(new_ops) == 1 and new_ops[0] == op:
+        return [op]
+    return list(itertools.chain.from_iterable(map(_decompose_gate_op, new_ops)))
+
+
+def _decompose_unsupported_gates(circuit: cirq.Circuit) -> cirq.Circuit:
+    """
+    Decompose gates in a circuit that are not in the supported set.
+
+    Args:
+        circuit (cirq.Circuit): The quantum circuit to process.
+
+    Returns:
+        cirq.Circuit: A new circuit with unsupported gates decomposed.
+    """
+    new_circuit = cirq.Circuit()
+    for moment in circuit:
+        new_ops = []
+        for op in moment:
+            if isinstance(op, cirq.GateOperation):
+                decomposed_ops = _decompose_gate_op(op)
+                new_ops.extend(decomposed_ops)
+            else:
+                new_ops.append(op)
+
+        new_circuit.append(new_ops)
+    return new_circuit
+
+
+def preprocess_circuit(circuit: cirq.Circuit) -> cirq.Circuit:
+    """
+    Preprocesses a Cirq circuit to ensure that it is compatible with the QIR conversion.
+
+    Args:
+        circuit (cirq.Circuit): The Cirq circuit to preprocess.
+
+    Returns:
+        cirq.Circuit: The preprocessed Cirq circuit.
+
+    """
+    circuit = _decompose_unsupported_gates(circuit)
+    qprogram = qbraid.programs.cirq.CirqCircuit(circuit)
+    qprogram._convert_to_line_qubits()
+    cirq_circuit = qprogram.program
+    return cirq_circuit
---
+++ qbraid_qir/serialization.py
@@ -0,0 +1,64 @@
+# Copyright (C) 2023 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module for exporting QIR to bitcode and LLVM IR files.
+
+"""
+import logging
+import os
+import pathlib
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    import pyqir
+
+
+def dumps(module: "pyqir.Module", output_dir: Optional[str] = None) -> None:
+    """
+    Saves the Quantum Intermediate Representation (QIR) of a given module
+    to bitcode (.bc) and LLVM IR (.ll) files.
+
+    Args:
+        module: The module containing the QIR.
+        output_dir (Optional[str]): The directory where the files will be saved.
+                                    If not provided, defaults to the current
+                                    working directory.
+
+    Raises:
+        Exception: If there's an error in writing the files.
+    """
+    # Determine the filename prefix from the module's source filename
+    filename_prefix = os.path.splitext(os.path.basename(module.source_filename))[0]
+
+    # Set the output directory to the current working directory if not provided
+    file_dir = output_dir if output_dir else str(pathlib.Path.cwd())
+
+    # Create the directory if it doesn't exist
+    os.makedirs(file_dir, exist_ok=True)
+
+    # Construct file paths
+    bc_file = os.path.join(file_dir, f"{filename_prefix}.bc")
+    ll_file = os.path.join(file_dir, f"{filename_prefix}.ll")
+
+    try:
+        # Save bitcode file (does not require an encoding)
+        with open(bc_file, "wb") as f:  # pylint: disable=unspecified-encoding
+            f.write(module.bitcode)
+        logging.info("Saved to %s", bc_file)
+
+        # Save LLVM IR file (default python encoding)
+        with open(ll_file, "w", encoding="utf-8") as f:
+            f.write(str(module))
+        logging.info("Saved to %s", ll_file)
+
+    except Exception as e:
+        logging.error("An error occurred: %s", e)
+        raise
