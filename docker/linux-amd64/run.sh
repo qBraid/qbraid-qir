97b6b30bfb69e4f22fc06d6e605fb0ba7ddc7cb4
Ryan Hill <ryanjh88@gmail.com>
2024-10-21 14:47:49 -0500

    update run run container echo

--- docker/linux-amd64/run.sh
+++ docker/linux-amd64/run.sh
@@ -15,7 +15,7 @@ if [ -n "$CONTAINER_NAME" ]; then
   docker run --platform linux/amd64 --name $CONTAINER_NAME -d qbraid/qir-runner tail -f /dev/null > /dev/null 2>&1
   echo "Container $CONTAINER_NAME started in detached mode."
 else
-  echo "Starting unnamed container in detached mode..."
-  CONTAINER_ID=$(docker run --platform linux/amd64 -d qbraid/qir-runner tail -f /dev/null > /dev/null 2>&1)
-  echo "Started container with ID: $CONTAINER_ID in detached mode."
+  echo "Starting container in detached mode..."
+  docker run --platform linux/amd64 -d qbraid/qir-runner tail -f /dev/null > /dev/null 2>&1
+  echo "Container started in detached mode."
 fi
